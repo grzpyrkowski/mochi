40069a12a5ec1773c04b72631618c499712a0e83
Grzegorz Pyrkowski <grz.pyrkowski@gmail.com>
2024-06-26 01:03:29 +0200

    Font

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,17 @@
+import piekny from "../data/pictures/piekny.jpg";
+
 export default function LandingPage() {
     return (
         <>
             <section className="bg-mochi-gray text-mochi-orange text-8xl">
-                <h1 className="font-gloock">Mochi</h1>
+                <h1 className="s">mo.chi</h1>
+            </section>
+            <section className="bg-mochi-gray">
+                <div className="fan mx-8 my-5 flex">
+                    <img className="fan-photo" src={piekny} alt="piekny"/>
+                    <img className="fan-photo" src={piekny} alt="piekny"/>
+                    <img className="fan-photo" src={piekny} alt="piekny"/>
+                </div>
             </section>
         </>
     )
